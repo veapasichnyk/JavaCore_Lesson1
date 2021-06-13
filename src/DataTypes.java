139e569d142c8d384cac8c190ec08d39acf3c41b
vapasichnyk <69596614+vapasichnyk@users.noreply.github.com>
2021-06-13 16:51:20 +0300

    Initial commit

---
+++ src/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String arg[]){
+        int a = 2;
+        double b = 2.0;
+        float c = 2F;
+        byte d = 2;
+        short e = 2;
+        long f = 2L;
+        char g = 'f';
+        boolean h = true;
+
+        System.out.println("int" + " " + a + "\ndouble" + " " + b +
+                           "\nfloat" + " " + c + "\nbyte" + " " + d +
+                           "\nshort" + " " + e + "\nlong" + " " + f +
+                           "\nchar" + " " + g + "\nboolean" + " " + h);
+
+
+    }
+}
